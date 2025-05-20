039ea48770c9330d5822de716b0c9e76dc329caf
Cristian Urcullu <cr.urcullu@duocuc.cl>
2025-05-19 23:13:19 -0400

    Buscar por id

--- src/main/java/com/edutech/msreportes/controller/reporteController.java
+++ src/main/java/com/edutech/msreportes/controller/reporteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.edutech.msreportes.model.Reporte;
 import com.edutech.msreportes.service.ReporteService;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("api/reportes")
@@ -40,4 +43,11 @@ public class ReporteController {
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reporte> getById(@PathVariable int idReporte) {
+        return new ResponseEntity<Reporte>(reporteService.findById(idReporte), HttpStatus.OK);
+    }
+    
+    
 }
